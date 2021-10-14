93d8832c0fffc3b92d9901265fa7e9af405ad416
Adrici <cirellia5@gmail.com>
2021-10-14 14:51:50 -0300

    Set up

---
+++ site-viagens/site-viagens/src/test/java/treino/com/br/siteviagens/SiteViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package treino.com.br.siteviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
